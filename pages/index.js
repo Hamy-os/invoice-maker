358f0a84f915c019ec3c29cf358e8005093e0256
Hamy <64610533+Hamy-os@users.noreply.github.com>
2022-02-04 14:59:30 +0300

    oops again

--- pages/index.js
+++ pages/index.js
@@ -6,31 +6,28 @@ import { useState } from 'react'
 import download from 'downloadjs'
 
 export default function Home() {
-  const [invoiceNumber, setIN] = useState("")
-  const [contractNumber, setCN] = useState("")
-  const [quantityField, setQ] = useState("")
-  const [descriptionField, setD] = useState("")
-  const [priceField, setP] = useState("")
-  const [phoneField, setPF] = useState("")
-  const [adressField, setAF] = useState("")
+  const [IN, setIN] = useState("")
+  const [CN, setCN] = useState("")
+  const [Q, setQ] = useState("")
+  const [D, setD] = useState("")
+  const [P, setP] = useState("")
+  const [PF, setPF] = useState("")
+  const [AF, setAF] = useState("")
+  const [EF, setEF] = useState("")
   const { PDFDocument } = require('pdf-lib')
-  const [emailField, setEF] = useState("")
 
-  const handleSubmit = (event, IN,CN,Q,D,P,PF,AF,EF) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     console.log('rendering pdf')
+    console.log(IN + CN + Q + D + P + PF + AF + EF)
     fillForm(IN,CN,Q,D,P,PF,AF,EF)
   }
 
   async function fillForm(IN,CN,Q,D,P,PF,AF,EF) {
-    const formUrl = 'https://i.file.glass/uwJ5gaW2gi.pdf'
-    const formPdfBytes = await fetch('https://i.file.glass/uwJ5gaW2gi.pdf').then(res => res.arrayBuffer())
-      // Load a PDF with form fields
-    const pdfDoc = await PDFDocument.load(formPdfBytes)
-
-      // Get the form containing all the fields
+      const formUrl = 'https://raw.githubusercontent.com/Hamy-os/invoice-maker/main/public/pdfbase.pdf'
+      const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer())
+      const pdfDoc = await PDFDocument.load(formPdfBytes)
       const form = pdfDoc.getForm()
-
       const invoiceNumber = form.getTextField('invoice-number')
       const contractNumber = form.getTextField('contract-number')
       const quantityField = form.getTextField('q')
@@ -49,68 +46,92 @@ export default function Home() {
       phoneField.setText(PF)
       adressField.setText(AF)
       emailField.setText(EF)
-      // Serialize the PDFDocument to bytes (a Uint8Array)
-      const pdfBytes = await pdfDoc.save()
 
-			// Trigger the browser to download the PDF document
+      const pdfBytes = await pdfDoc.save()
       download(pdfBytes, "pdf-lib_form_creation_example.pdf", "application/pdf");
   }
 
   return (
     <div>
-      <Head>
-      <script src="https://cdn.tailwindcss.com"></script>
-      <script src="https://unpkg.com/downloadjs@1.4.7"></script>
-      </Head>
-      <h1>Please fill in these fields:</h1>
-    
-    <form onSubmit={handleSubmit}>
-      <div className="mb-6">
-        <label className="block mb-2 text-sm font-medium text-gray-900 ">Invoice Number:</label>
-        <input type="text" value={invoiceNumber} onChange={(e) => setIN(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="00001" required=""/>
+      <h1 className="text-center m-10">Please fill in these fields:</h1>
+
+      <form onSubmit={handleSubmit}>
+    <div className="bg-white shadow rounded-lg p-6">
+    <div className="grid lg:grid-cols-2 gap-6">
+      <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+      <label className="bg-white text-gray-600 px-1">
+        Invoice Number:
+      </label>
+      <p>
+        <input type="text" value={IN} onChange={(e) => setIN(e.target.value)} className="py-1 px-1 text-gray-900 outline-none block h-full w-full" placeholder="00001" required=""/>
+      </p>
       </div>
-      <div className="mb-6">
-        <label className="block mb-2 text-sm font-medium text-gray-900 ">Contract Number:</label>
-        <input type="text" value={contractNumber} onChange={(e) => setCN(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required=""/>
-      </div>
-      <div className="mb-6">
-        <label className="block mb-2 text-sm font-medium text-gray-900 ">
-          Quantity:
-        </label>
-        <input type="text" value={quantityField} onChange={(e) => setQ(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required=""/>
-      </div>
-      <div className="mb-6">
-        <label className="block mb-2 text-sm font-medium text-gray-900 ">
-          Description:
-        </label>
-        <input type="text" value={descriptionField} onChange={(e) => setD(e.target.value)}  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required=""/>
-      </div>
-      <div className="mb-6">
-        <label className="block mb-2 text-sm font-medium text-gray-900 ">
-          Price:
-        </label>
-        <input type="text" value={priceField} onChange={(e) => setP(e.target.value)}  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required=""/>
-      </div>
-      <div className="mb-6">
-        <label className="block mb-2 text-sm font-medium text-gray-900 ">
-          Your Phone Number:
-        </label>
-        <input type="text" value={phoneField} onChange={(e) => setPF(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required=""/>
-      </div>
-      <div className="mb-6">
-        <label className="block mb-2 text-sm font-medium text-gray-900 ">
-          Your Adress:
-        </label>
-        <input type="text" value={adressField} onChange={(e) => setAF(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required=""/>
-      </div>
-      <div className="mb-6">
-        <label className="block mb-2 text-sm font-medium text-gray-900 ">
+      <div className="bg-grey-500 border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+        <label className=" text-gray-600 px-1">
           Your Email:
         </label>
-        <input type="text" value={emailField} onChange={(e) => setEF(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required=""/>
+        <p>
+          <input type="email" value={EF} onChange={(e) => setEF(e.target.value)} className="bg-grey-500 py-1 px-1 text-gray-900 outline-none block h-full w-full" placeholder="example@example.com" required=""/>
+        </p>
       </div>
-    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
-    </form>
+      <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+      <label className="bg-white text-gray-600 px-1">
+          Your Adress:
+        </label>
+        <p>
+        <input type="text" value={AF} onChange={(e) => setAF(e.target.value)} className="py-1 px-1 text-gray-900 outline-none block h-full w-full" placeholder="City Center Doha Qatar" required=""/>
+        </p>
+      </div>
+      <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+      <label className="bg-white text-gray-600 px-1">
+          Your Phone Number:
+        </label>
+        <p>
+        <input type="text" value={PF} onChange={(e) => setPF(e.target.value)} className="py-1 px-1 text-gray-900 outline-none block h-full w-full" placeholder="5050 5099" required=""/>
+        </p>
+      </div>
+      <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+      <label className="bg-white text-gray-600 px-1">
+          Price:
+        </label>
+        <p>
+        <input type="text" value={P} onChange={(e) => setP(e.target.value)}  className="py-1 px-1 text-gray-900 outline-none block h-full w-full" placeholder="600" required=""/>
+        </p>
+        </div>
+      <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+      <label className="bg-white text-gray-600 px-1">
+          Description:
+        </label>
+        <p>
+        <input type="text" value={D} onChange={(e) => setD(e.target.value)}  className="py-1 px-1 text-gray-900 outline-none block h-full w-full" placeholder="Tour guide service provided for discover qatar (MSC) 1st January 2022" required=""/>
+        </p>
+        </div>
+      <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+      <label className="bg-white text-gray-600 px-1">
+          Quantity:
+        </label>
+        <p>
+          <input type="text" value={Q} onChange={(e) => setQ(e.target.value)} className="py-1 px-1 text-gray-900 outline-none block h-full w-full" placeholder="1" required=""/>
+        </p>
+      </div>
+      <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+      <label className="bg-white text-gray-600 px-1">
+        Contract Number:
+      </label>
+      <p>
+        <input type="text" value={CN} onChange={(e) => setCN(e.target.value)} className="py-1 px-1 text-gray-900 outline-none block h-full w-full" placeholder="QTR-CNT-2022-0001" required=""/>
+      </p>
+      </div>
+
+    </div>
+    <div className="border-t mt-6 pt-3">
+      <button  type="submit" className="rounded text-gray-100 px-3 py-1 bg-blue-500 hover:shadow-inner hover:bg-blue-700 transition-all duration-300">
+        Save
+      </button>
+    </div>
+  </div>
+  </form>
+  
 
     </div>
   )
